6b9388847615a3970d911c294329a6ddb28244aa
nmadariaga <tali-tali@openmailbox.org>
2015-07-24 21:32:20 -0300

    Primer commit

---
+++ damas/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: neftali
+ *
+ * Created on 24 de julio de 2015, 21:28
+ */
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "ola k ase" << endl;
+    return 0;
+}
+
